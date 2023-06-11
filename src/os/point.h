4e18a10dc0de2e7e2285a710633a48a31865a7b6
doomed-phobos <the_ekj.2121@outlook.com>
2023-06-11 09:14:20 -0500

    Added Point class

---
+++ src/os/point.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <type_traits>
+
+namespace os {
+   template<typename T,
+      std::enable_if_t<std::is_arithmetic_v<T>, int> = 0>
+   struct PointT {
+      PointT() :
+         x{}, y{} {}
+      PointT(const T& x, const T& y) :
+         x{x}, y{y} {}
+      template<typename U,
+         std::enable_if_t<std::is_convertible_v<U, T>, int> = 0>
+      PointT(const PointT<U>& pt) :
+         x{pt.x}, y{pt.y} {}
+
+      PointT& operator+=(const PointT& pt) {
+         x += pt.x;
+         y += pt.y;
+         return *this;
+      }
+
+      PointT operator+(const PointT& pt) const {
+         return {x + pt.x, y + pt.y};
+      }
+
+      T x, y;
+   };
+
+   typedef PointT<int> Point;
+} // namespace os
